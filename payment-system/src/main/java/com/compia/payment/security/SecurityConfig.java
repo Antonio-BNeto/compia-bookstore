4188c016d4cbef70a88a8968d5cd59e7dec54fac
GbrlGrmno <gabri1020m@gmail.com>
2025-09-26 20:30:27 -0300

    fix: Add bean annotation to SecurityFilterChain

--- payment-system/src/main/java/com/compia/payment/security/SecurityConfig.java
+++ payment-system/src/main/java/com/compia/payment/security/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 public class SecurityConfig {
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
